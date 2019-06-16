afc0fe08d15b756a1629afbec5b1362ed818eee2
spelhate <sebastien.pelhate@bretagne.bzh>
2019-06-16 16:52:15 +0200

    Add vuetify to the project

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
 <template>
-  <div class="container">
-    <h1>{{ message }}</h1>
+  <div class="container">    
+	<v-toolbar>
+		<v-toolbar-side-icon></v-toolbar-side-icon>
+		<v-toolbar-title>{{ message }}</v-toolbar-title>
+		<v-spacer></v-spacer>
+		<v-toolbar-items class="hidden-sm-and-down">
+		  <v-btn flat>Link One</v-btn>
+		  <v-btn flat>Link Two</v-btn>
+		  <v-btn flat>Link Three</v-btn>
+		</v-toolbar-items>
+	  </v-toolbar>
     <olMap></olMap>
+	<div class="text-xs-center">
+		<v-btn fab dark small color="primary">
+		  <v-icon dark>remove</v-icon>
+		</v-btn>
+
+		<v-btn fab dark small color="pink">
+		  <v-icon dark>favorite</v-icon>
+		</v-btn>
+
+		<v-btn fab dark color="indigo">
+		  <v-icon dark>add</v-icon>
+		</v-btn>
+
+		<v-btn fab dark color="teal">
+		  <v-icon dark>list</v-icon>
+		</v-btn>
+
+		<v-btn fab dark large color="cyan">
+		  <v-icon dark>edit</v-icon>
+		</v-btn>
+
+		<v-btn fab dark large color="purple">
+		  <v-icon dark>android</v-icon>
+		</v-btn>
+  </div>
   </div>
 </template>
 
@@ -19,7 +53,8 @@ export default {
 </script>
 
 <style scoped>
-  .container {
+  .container {	
+    font-family: Roboto;
     width: 100%;
     height: 100%;
   }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App'
+import Vuetify from 'vuetify'
+
+import 'vuetify/dist/vuetify.min.css'
+
+Vue.use(Vuetify)
 
 new Vue({
   el: '#app',
